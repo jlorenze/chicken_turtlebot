f7d0e6a2844163597295511fffb190d791c19a21
Joe Lorenzetti <jlorenze@stanford.edu>
2018-03-15 13:37:47 -0700

    changing to tf

--- scripts/objectMapping.py
+++ scripts/objectMapping.py
@@ -4,6 +4,7 @@ import rospy
 import tf
 import numpy as np
 from chicken_turtlebot.msg import DetectedObject, DetectedStopSign, DetectedAnimal
+from geometry_msgs.msg import PoseStamped
 import pdb
 
 def wrapToPi(a):
@@ -29,9 +30,12 @@ class ObjectMapping:
         self.animalCounts = []
         self.animalPublisher = rospy.Publisher('/animals', DetectedAnimal, queue_size=10)
 
+        self.animal_pos_pub = rospy.Publisher('/animal_pos', PoseStamped, queue_size=10)
 
         self.tf_listener = tf.TransformListener()
         rospy.Subscriber('/detector/stop_sign', DetectedObject, self.stop_sign_detected_callback)
+
+        self.tf_broadcaster = tf.TransformBroadcaster()
 
     def searchForTopics(self):
         topics = rospy.get_published_topics(namespace='/detector')
@@ -51,8 +55,8 @@ class ObjectMapping:
         r = dx/dy # aspect ratio
 
         # Use linear interpolation from test data
-        rdist = np.array([.15, .20, .25, .30,.35, .40, .45, .50])
-        pixelheight = np.array([139, 102, 82, 64, 56, 50, 44, 40])
+        rdist = np.array([.26, .31, .36, .41, .46, .51, .56])
+        pixelheight = np.array([120, 108, 95, 86, 71, 67, 65])
         if dy > pixelheight[-1] and dy < pixelheight[0]:
             dist = np.interp(dy, pixelheight[::-1], rdist[::-1])
         else:
@@ -102,12 +106,35 @@ class ObjectMapping:
             self.animals[2].append(animal)
             self.animalCounts.append(1)
 
-        #  Publishes the detected object and its location
-        object_msg = DetectedAnimal()
-        object_msg.x = self.animals[0]
-        object_msg.y = self.animals[1]
-        object_msg.animal = self.animals[2]
-        self.animalPublisher.publish(object_msg)
+
+        # Publish locations to TF tree
+        for i in range(len(self.animals[0])):
+            x = self.animals[0][i]
+            y = self.animals[1][i]
+            z = 0.
+            animal = self.animals[2][i]
+            self.tf_broadcaster.sendTransform((x,y,z), tf.transformations.quaternion_from_euler(0,0,0),
+                                    rospy.Time.now(),animal,'map')
+
+        # #  Publishes the detected object and its location
+        # object_msg = DetectedAnimal()
+        # object_msg.x = self.animals[0]
+        # object_msg.y = self.animals[1]
+        # object_msg.animal = self.animals[2]
+        # self.animalPublisher.publish(object_msg)
+
+        # # publish current desired x and y for visualization only
+        # pathsp_msg = PoseStamped()
+        # pathsp_msg.header.frame_id = 'map'
+        # pathsp_msg.pose.position.x = self.animals[0][0]
+        # pathsp_msg.pose.position.y = self.animals[1][0]
+        # theta_d = 0.0
+        # quat_d = tf.transformations.quaternion_from_euler(0, 0, theta_d)
+        # pathsp_msg.pose.orientation.x = quat_d[0]
+        # pathsp_msg.pose.orientation.y = quat_d[1]
+        # pathsp_msg.pose.orientation.z = quat_d[2]
+        # pathsp_msg.pose.orientation.w = quat_d[3]
+        # self.animal_pos_pub.publish(pathsp_msg)
 
     def stop_sign_detected_callback(self, msg):
         """ callback for when the detector has found a stop sign. Note that
