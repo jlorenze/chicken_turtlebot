7209c1391cadaed2cb61c43034b6f17f11093ce7
Joe Lorenzetti <jlorenze@stanford.edu>
2018-03-16 03:37:30 -0700

    boring

--- scripts/testing.py
+++ scripts/testing.py
@@ -2,7 +2,7 @@
 
 import rospy
 from gazebo_msgs.msg import ModelStates
-from std_msgs.msg import Float32MultiArray, String
+from std_msgs.msg import Float32MultiArray, String, Bool
 from geometry_msgs.msg import Twist, PoseArray, Pose2D, PoseStamped
 from chicken_turtlebot.msg import DetectedObject
 from matplotlib import pyplot as plt
@@ -20,54 +20,19 @@ class Testing:
 
     def __init__(self):
         rospy.init_node('turtlebot_tester', anonymous=True)
-        rospy.Subscriber('/detector/stop_sign', DetectedObject, self.stop_sign_detected_callback)
-        self.trans_listener = tf.TransformListener()
+        self.start = rospy.Publisher('/explore_yn', Bool, queue_size=10)
 
-    def stop_sign_detected_callback(self, msg):
-        """ callback for when the detector has found a stop sign. Note that
-        a distance of 0 can mean that the lidar did not pickup the stop sign at all """
-
-        # distance of the stop sign
-        corners = msg.corners
-        dx = corners[3] - corners[1]
-        dy = corners[2] - corners[0]
-
-        r = dx/dy # aspect ratio
-
-        rdist = np.array([.15, .20, .25, .30,.35, .40, .45, .50])
-        pixelheight = np.array([139, 102, 82, 64, 56, 50, 44, 40])
-        if dy > pixelheight[-1] and dy < pixelheight[0]:
-            dist = np.interp(dy, pixelheight[::-1], rdist[::-1])
-        else:
-            dist = 0
-
-        # Get location of camera with respect to the map
-        try:
-            (translation,rotation) = self.trans_listener.lookupTransform('/map', '/camera', rospy.Time(0))
-            xcam = translation[0]
-            ycam = translation[1]
-            zcam = translation[2]
-            euler = tf.transformations.euler_from_quaternion(rotation)
-            thetacam = euler[2]
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-            return
-
-        # Now we have pose of robot, we want to determine stop sign angle relative
-        # to camera frame
-        thstopsign = (wrapToPi(msg.thetaright) + wrapToPi(msg.thetaleft))/2.
-        zstopsign = dist*np.cos(-thstopsign)
-        xstopsign = dist*np.sin(-thstopsign)
-
-        x = xcam + xstopsign*np.cos(thetacam) - zstopsign*np.sin(thetacam) 
-        y = ycam + xstopsign*np.sin(thetacam) + zstopsign*np.cos(thetacam)
-
-
+    def startExplorer(self):
+        object_msg = Bool()
+        object_msg.data = True
+        self.start.publish(object_msg)
 
     def run(self):
         rate = rospy.Rate(10) # 10 Hz
         while not rospy.is_shutdown():
+            self.startExplorer()
             rate.sleep()
-
+        
 
 if __name__ == '__main__':
     sup = Testing()
